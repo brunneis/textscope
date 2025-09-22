60b9b9b65eab230a3adda5584f2dcd8fdf7ca24c
MarcosFP97 <marcosfernandez.pichel@usc.es>
2025-09-22 13:32:55 +0200

    v0.1.8

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import io
 
 setup(
     name="textscope",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     package_data={
         'textscope': ['data/config.yaml'],
@@ -15,10 +16,10 @@ setup(
         "pyyaml",
         "nltk"
     ],
+    license="GNU",  # o la que uses
+    license_files=["LICENSE"],
     include_package_data=True,
     description="A Python text analysis library for relevance and subtheme detection",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
     author="Marcos Fernández-Pichel",
     author_email="marcosfernandez.pichel@usc.es",
 )
